651aa87bc829ff5081e188c06fc91ef50510d5d0
vsreyes <virgilio.s.reyes@gmail.com>
2021-09-30 21:52:08 +0200

    Layout setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons';
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+          <Avatar />
+          <Typography.Title level={2} className="logo">
+            <Link to="/">Cryptoverse</Link>
+          </Typography.Title>
+          {/*<Button className="menu-control-container">
+
+          </Button>*/}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
