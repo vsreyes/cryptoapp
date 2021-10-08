ac1c5f429185ecb0170eb378d9498cedbb486575
vsreyes <virgilio.s.reyes@gmail.com>
2021-10-08 16:05:36 +0200

    Redux setup

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+var options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/stats',
+  headers: {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '23a75a1255msh99f5f0760d8aaf5p141db4jsnf7a5d579ca91'
+  }
+};
