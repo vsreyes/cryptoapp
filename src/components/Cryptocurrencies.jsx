514736a3430f0beb2229b42bc96f2b7beedbc7e6
vsreyes <virgilio.s.reyes@gmail.com>
2021-10-15 20:46:51 +0200

    Crypto

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -6,6 +6,7 @@ import { Card, Row, Col, Input } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const Cryptocurrencies = () => {
+  
   const { data: cryptosList, isFetching } = useGetCryptosQuery();
   const [ cryptos, setCryptos ] = useState(cryptosList?.data?.coins);
 
@@ -14,9 +15,9 @@ const Cryptocurrencies = () => {
   return (
     <>
       <Row gutter={[32,32]} className="crypto-card-container">
-          {cryptos.map((currency) => {
+          {cryptos?.map((currency) => (
             <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-                <Link to={`/crypto/${currency.id}`}>
+                <Link key={currency.id} to={`/crypto/${currency.id}`}>
                   <Card 
                     title={`${currency.rank}. ${currency.name}`}
                     extra={<img className="crypto-image" src={currency.iconUrl} />}
@@ -28,7 +29,7 @@ const Cryptocurrencies = () => {
                   </Card>
                 </Link>
             </Col>
-          })}
+          ))}
       </Row>
     </>
   )
