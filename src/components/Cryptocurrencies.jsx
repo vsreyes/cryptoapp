bbf4186e50c68276f4eaaf9f7c6e71ba830f03f9
vsreyes <virgilio.s.reyes@gmail.com>
2021-10-16 16:05:12 +0200

    Crypto news setup

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -6,7 +6,7 @@ import { Card, Row, Col, Input } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const Cryptocurrencies = ({simplified}) => {
-  const count = simplified ? 10 : 100
+  const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [ cryptos, setCryptos ] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
@@ -21,9 +21,11 @@ const Cryptocurrencies = ({simplified}) => {
 
   return (
     <>
+    {!simplified && (
       <div className="search-crypto">
-        <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+        <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value.toLowerCase())} />
       </div>
+    )}
       <Row gutter={[32,32]} className="crypto-card-container">
           {cryptos?.map((currency) => (
             <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
