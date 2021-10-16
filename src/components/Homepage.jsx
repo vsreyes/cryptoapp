3c72c1ff75429455bda135d079359f013c46ce30
vsreyes <virgilio.s.reyes@gmail.com>
2021-10-16 16:59:56 +0200

    Corrections

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,7 +4,8 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
-import { Cryptocurrencies, News } from '../components'
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
 
 const { Title } = Typography;
 
@@ -17,11 +18,12 @@ const Homepage = () => {
   return (
     <>
       <Title level={2} className="heading">Global Crypto Stats</Title>
-      <Row> 
+      <Row gutter={[32, 32]}>
         <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
         <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-        <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
       <div className="home-heading-container">
