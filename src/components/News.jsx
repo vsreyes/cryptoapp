28080c34f9832d278e80d9c4f8260d06f75c714d
vsreyes <virgilio.s.reyes@gmail.com>
2021-10-19 14:36:22 +0200

    News setup

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,8 +7,10 @@ import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
 const News = ({ simplified }) => {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100 }) 
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 }) 
 
   if(!cryptoNews?.value) return 'Loading...';
 
@@ -17,8 +19,20 @@ const News = ({ simplified }) => {
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
-            <a href={news.url} target="_blank" ref="noreferrer">
-              
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="name-title" level={4}>{news.name}</Title>
+                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+              </div>
+              <p>
+                {news.description > 100 ? `${news.description.substring(0, 100)}...`: news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                  <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                </div>
+              </div>
             </a>
           </Card>
         </Col>
