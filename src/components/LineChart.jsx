a8b47627780eda788e69285a14244e0d9d2cbd51
vsreyes <virgilio.s.reyes@gmail.com>
2021-11-01 15:27:57 +0100

    Crypto details setup

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -5,11 +5,19 @@ import { Col, Row, Typography } from 'antd';
 const { Title } = Typography;
 
 
-const LineChart = () => {
-  return (
-    <div>
-      
-    </div>
+const LineChart = ({ coinHistory, currentPrice, coinName}) => {
+  return ( 
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">{coinName} Price Chart</Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            {coinHistory?.data?.change}%</Title>
+          <Title level={5} className="current-price">
+            </Title>
+        </Col>
+      </Row> 
+    </>
   )
 }
 
