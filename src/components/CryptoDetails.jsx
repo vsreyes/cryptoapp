e270f9c94052f5895137b32504832a9e3a18948f
vsreyes <virgilio.s.reyes@gmail.com>
2021-11-03 14:47:06 +0100

    Correction

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -53,7 +53,7 @@ const CryptoDetails = () => {
       <Select defaultValue="7d" className="select-timeperiod" placeholder="Select Time Period" onChange={(value) => setTimePeriod(value)}>
       {time.map((date) => <Option key={date}>{date}</Option>)}
       </Select>
-      <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name}/>
+      <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name} />
       <Col className="stats-container">
         <Col className="coin-value-statistics">
         <Col className="coin-value-statistics-heading">
