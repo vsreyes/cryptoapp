267826a9ae46da0bc063d01b6bfec58ad5c03aea
vsreyes <virgilio.s.reyes@gmail.com>
2021-11-03 14:40:01 +0100

    Chart setup

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -4,7 +4,6 @@ import { Col, Row, Typography } from 'antd';
 
 const { Title } = Typography;
 
-
 const LineChart = ({ coinHistory, currentPrice, coinName}) => {
   const coinPrice = [];
   const coinTimestamp = [];
@@ -12,7 +11,31 @@ const LineChart = ({ coinHistory, currentPrice, coinName}) => {
   for (let i = 0; coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory.data.history[i].price)
     coinTimestamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDataString())
+  }
 
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price in USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd'
+      }
+    ]
+  }
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
   }
 
   return ( 
