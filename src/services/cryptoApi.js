78ada95862316891e15461a0d71ec0e633522d05
vsreyes <virgilio.s.reyes@gmail.com>
2021-11-07 13:44:06 +0100

    Challenges

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -17,7 +17,7 @@ export const cryptoApi = createApi({
       query: count => createRequest(`/coins?limit=${count}`),
     }),
     getExchanges: builder.query({
-      query: count => createRequest(`/exchanges`),
+      query: () => createRequest(`/exchanges`),
     }),
     getCryptoDetails: builder.query({
       query: coinId => createRequest(`/coin/${coinId}`),
@@ -31,6 +31,7 @@ export const cryptoApi = createApi({
 
 export const {
   useGetCryptosQuery,
+  useGetExchangesQuery,
   useGetCryptoDetailsQuery,
   useGetCryptoHistoryQuery,
 } = cryptoApi;
